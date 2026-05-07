f0ee8ef2b0a082a27271ba852d9b8bfac305b21e
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-07 20:43:07 -0300

    Update Missão 3 - A aliança dos triângulos.py

--- Códigos/Missão 3 - A aliança dos triângulos.py
+++ Códigos/Missão 3 - A aliança dos triângulos.py
@@ -1,1 +1,3 @@
+#Desenvolva uma função que desenhe triângulos isósceles com base e altura fornecidas pelo usuário. Em seguida, use essa função para desenhar pelo menos 5 triângulos com diferentes tamanhos e posições em tela. 
+#Cada triângulo deve ter uma cor distinta.
 
