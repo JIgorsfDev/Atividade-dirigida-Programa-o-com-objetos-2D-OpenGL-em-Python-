a37af336cfaac90c725d214e4399ffb273133eb1
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-07 20:43:37 -0300

    Add scene with three triangles and coordinate labels

--- Códigos/Missão 4 - O Domínio do espaço.py
+++ Códigos/Missão 4 - O Domínio do espaço.py
@@ -1,1 +1,2 @@
-
+#Crie uma cena com pelo menos três triângulos posicionados em diferentes quadrantes do plano cartesiano (SRU), e desenhe os eixos X e Y. Exiba um pequeno texto (usando glutBitmapCharacter ou similar) 
+#com as coordenadas de cada triângulo.
