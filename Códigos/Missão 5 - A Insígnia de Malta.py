57bb5b50e61c241549fe221c8f16ed35db7aab30
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-08 14:04:55 -0300

    Update Missão 5 - A Insígnia de Malta.py

--- Códigos/Missão 5 - A Insígnia de Malta.py
+++ Códigos/Missão 5 - A Insígnia de Malta.py
@@ -1,3 +1,6 @@
+#Reconstruir um antigo símbolo conhecido como "Insígnia de Malta", usado para desbloquear o “Portal dos Pixels”. Para isso, crie uma função chamada desenharInsignia() que monte uma figura simétrica utilizando
+#apenas retângulos e triângulos. Ao pressionar a tecla “ C” deve alterar as cores do símbolo aleatoriamente (como se ele ganhasse “energia mágica”).
+
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 import sys
